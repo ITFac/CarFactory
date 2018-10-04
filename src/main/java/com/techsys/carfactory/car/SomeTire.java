2fd49446c8a1aa1ca1866219e4431cc5db1751d7
Anoukh <anoukh.ashley@gmail.com>
2018-10-04 17:02:52 +0530

    Add new tire

---
+++ src/main/java/com/techsys/carfactory/car/SomeTire.java
@@ -0,0 +1,10 @@
+package com.techsys.carfactory.car;
+
+import com.techsys.carfactory.icar.Tire;
+
+public class SomeTire extends Tire {
+
+    public SomeTire(){
+        this.setMade("Some");
+    }
+}
